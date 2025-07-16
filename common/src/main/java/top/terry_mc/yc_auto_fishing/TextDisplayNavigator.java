f4c37a0a972d16b3da10adc362e204a870293f35
Terry-MC123 <terry_mc@qq.com>
2025-07-16 09:14:38 +0800

    fix:low detect

--- common/src/main/java/top/terry_mc/yc_auto_fishing/TextDisplayNavigator.java
+++ common/src/main/java/top/terry_mc/yc_auto_fishing/TextDisplayNavigator.java
@@ -69,7 +69,7 @@ public class TextDisplayNavigator {
 
         for (Entity entity : mc.level.entitiesForRendering()) {
             if (entity instanceof TextDisplay display) {
-                if(!display.textRenderState().text().getString().contains("鱼群")||display.textRenderState().text().getString().contains("库存：空")||display.textRenderState().text().getString().contains("库存: 空")) continue;
+                if(!display.textRenderState().text().getString().contains("鱼群")||display.textRenderState().text().getString().contains("库存：空")||display.textRenderState().text().getString().contains("库存: 空")||display.textRenderState().text().getString().contains("库存：少")||display.textRenderState().text().getString().contains("库存: 少")) continue;
 
                 int lineCount = getTextLineCount(display);
 
